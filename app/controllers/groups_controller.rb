f7cbe8e3c7b76fa2829ed986cef72dc4de2d98df
koukimiyauchi <kouki3850@gmail.com>
2020-07-01 21:47:16 +0900

    (終)メッセージ送信機能

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -22,8 +22,8 @@ class GroupsController < ApplicationController
 
   def update
     @group = Group.find(params[:id])
-    if @group.update(params)
-      redirect_to rootpath, notice: 'グループを更新しました'
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを更新しました'
     else
       render :edit
     end
--- app/models/group_user.rb
+++ app/models/group_user.rb
@@ -2,3 +2,5 @@ class GroupUser < ApplicationRecord
   belongs_to :group
   belongs_to :user
 end
+
+
